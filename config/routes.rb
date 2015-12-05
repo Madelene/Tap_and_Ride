19771d0ed4c70d88c55cfde0e2dad7b3be54a48d
Sean Sellek <me@seansellek.com>
2015-12-05 10:15:42 -0500

    replaced User routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :stations, except: [:new, :edit]
   resources :sessions, :only => [:create, :destroy]
   resources :trips, except: [:new, :edit]
-  
+  resources :users, except: [:new, :edit]
 end
